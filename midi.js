55f35a7d95f1fe0fde2945cdacb80e923fd2852c
Iain MacDonald <IJMacD@gmail.com>
2018-01-13 17:28:15 +0800

    Extract MIDI Event parsing

--- midi.js
+++ midi.js
@@ -17,8 +17,6 @@ class MidiNode {
 		const midi = parseMidi(this.buffer);
 		console.log(midi);
 
-		const tempo = 500000;	// Microseconds per quater note (500000 = 120 bpm)
-
 		const concertPitch = 440; // Note A4, 440 Hz
 		const concertPitchMidiNum = 0x45;
 		const noteRatio = Math.exp(Math.LN2 / 12);
@@ -30,6 +28,8 @@ class MidiNode {
 		midi.tracks.forEach(track => {
 
 			let nextTime = when;
+
+			const tempo = track.tempo || 500000;	// Microseconds per quater note (500000 = 120 bpm)
 
 			track.events.forEach(evt => {
 				nextTime += evt.deltaTime * ((tempo / midi.division) * 1e-6);
@@ -140,170 +140,54 @@ function parseMidi (buffer) {
 		}
 
 		const track = { events: [] };
-		let meta = track;
+		const meta = track;
 
 		let prevStatus;
+		let currTime = 0;
 
 		while (index < endIndex) {
 			const res = parseVariableLength(view, index);
 			const deltaTime = res.value;
 			index = res.index;
 
-			const status = view.getUint8(index++);
-			const channel = status & 0x0f;
+			currTime += deltaTime;
 
-			let key;
-			let velocity;
+			let status = view.getUint8(index++);
 
-			switch (status & 0xf0) {
-				case 0x80:
-				case 0x90:
-					key = view.getUint8(index++);
-					velocity = view.getUint8(index++);
-
-					track.events.push({ deltaTime, type: status & 0xf0, channel, key, velocity });
-
-					break;
-				case 0xA0:
-					key = view.getUint8(index++);
-					var pressure = view.getUint8(index++);
-
-					track.events.push({ deltaTime, type: status & 0xf0, channel, key, pressure });
-					break;
-				case 0xB0:
-					var controller = view.getUint8(index++);
-					var value = view.getUint8(index++);
-
-					track.events.push({ deltaTime, type: status & 0xf0, channel, controller, value });
-					break;
-				case 0xC0:
-					var program = view.getUint8(index++);
-
-					track.events.push({ deltaTime, type: status & 0xf0, channel, program });
-					break;
-				case 0xD0:
-					var pressure = view.getUint8(index++);
-
-					track.events.push({ deltaTime, type: status & 0xf0, channel, pressure });
-					break;
-				case 0xE0:
-					var value = view.getUint16(index);
-					index += 2;
-
-					track.events.push({ deltaTime, type: status & 0xf0, channel, value });
-					break;
-				case 0xF0:
-					switch (status) {
-						case 0xF0:
-						case 0xF7:
-							console.error("Sysex Events not supported");
-							break;
-						case 0xFF:
-							const type = view.getUint8(index++);
-							const length = view.getUint8(index++);
-							let text;
-							if (type !== 0x00 && type < 0x08) {
-								text = getAsciiText(view, index, length);
-							}
-							switch (type) {
-								case 0x00:
-									meta.sequenceNumber = view.getUint16(index);
-									break;
-								case 0x01:
-								case 0x05:
-								case 0x06:
-								case 0x07:
-									track.events.push({ deltaTime, type: status, text });
-									break;
-								case 0x02:
-									meta.copyright = text;
-									break;
-								case 0x03:
-									meta.name = text;
-									break;
-								case 0x04:
-									meta.instrument = text;
-									break;
-								case 0x20:
-									const channel = view.getUint8(index);
-									if (!track.channelMeta) {
-										track.channelMeta = {};
-									}
-									if (!track.channelMeta[channel]) {
-										track.channelMeta[channel] = {};
-									}
-									meta = track.channelMeta[channel];
-									break;
-							}
-							index += length;
-							break;
-						default:
-							throw new Error("Unsupported Event Type " +  "0x" + status.toString(16) + " at 0x" + index.toString(16));
-					}
-					break;
-				default:
-					if (status < 0x80) {
-						// Continuation - "Running Mode"
-						index--; // backtrack
-						const prevChannel = prevStatus & 0x0F;
-						switch (prevStatus & 0xF0) {
-							case 0x80:
-							case 0x90:
-								key = view.getUint8(index++);
-								velocity = view.getUint8(index++);
-								track.events.push({ deltaTime, type: prevStatus & 0xf0, channel: prevChannel, key, velocity });
-								break;
-							case 0xA0:
-								key = view.getUint8(index++);
-								var pressure = view.getUint8(index++);
-
-								track.events.push({ deltaTime, type: prevStatus & 0xf0, channel: prevChannel, key, pressure });
-								break;
-							case 0xB0:
-								var controller = view.getUint8(index++);
-								var value = view.getUint8(index++);
-
-								track.events.push({ deltaTime, type: prevStatus & 0xf0, channel: prevChannel, controller, value });
-								break;
-							case 0xC0:
-								var program = view.getUint8(index++);
-
-								track.events.push({ deltaTime, type: prevStatus & 0xf0, channel: prevChannel, program });
-								break;
-							case 0xD0:
-								var pressure = view.getUint8(index++);
-
-								track.events.push({ deltaTime, type: prevStatus & 0xf0, channel: prevChannel, pressure });
-								break;
-							case 0xE0:
-								var value = view.getUint16(index);
-								index += 2;
-
-								track.events.push({ deltaTime, type: prevStatus & 0xf0, channel: prevChannel, value });
-								break;
-							case 0xF0:
-								switch (status) {
-									case 0xF0:
-									case 0xF7:
-										console.error("Sysex Continuation Events not supported");
-										break;
-									default:
-										console.error("Continuation Meta events?");
-								}
-								break;
-							default:
-								throw new Error("Unsupported Continuation Event Type " +  "0x" + prevStatus.toString(16) + " at 0x" + index.toString(16));
-
-						}
-					}
-					else {
-						throw new Error("Unsupported Event Type " + "0x" + status.toString(16) + " at 0x" + (index-1).toString(16));
-					}
+			if (status < 0x80) {
+				status = prevStatus;
+				index--;
 			}
 
-			if (status & 0x80) {
-				prevStatus = status;
+			const eventRes = parseEvent(view, index, status);
+
+			if (eventRes.event) {
+				if (eventRes.event.type === 0xFF &&
+					eventRes.event.metaType === 0x2F)
+				{
+					// 0x2F: End of Track Meta Event
+					track.length = currTime;
+				}
+				else {
+					eventRes.event.deltaTime = deltaTime;
+					track.events.push(eventRes.event);
+				}
 			}
+
+			if (eventRes.meta) {
+				if (eventRes.meta.channel) {
+					if (!track.channelMeta) {
+						track.channelMeta = {};
+					}
+					if (!track.channelMeta[eventRes.meta.channel]) {
+						track.channelMeta[eventRes.meta.channel] = {};
+					}
+					meta = track.channelMeta[eventRes.meta.channel];
+				}
+				Object.assign(meta, eventRes.meta);
+			}
+
+			index = eventRes.index;
 		}
 
 		trackNo++;
@@ -325,6 +209,141 @@ function parseVariableLength (dataView, index) {
 	value += byte;
 
 	return { value, index };
+}
+
+function parseEvent (view, index, status) {
+	const type = status &0xf0;
+	const channel = status & 0x0f;
+
+	let event;
+	let meta;
+
+	let key;
+	let velocity;
+
+	switch (status & 0xf0) {
+		case 0x80:
+		case 0x90:
+			key = view.getUint8(index++);
+			velocity = view.getUint8(index++);
+
+			event = { type, channel, key, velocity };
+
+			break;
+		case 0xA0:
+			key = view.getUint8(index++);
+			var pressure = view.getUint8(index++);
+
+			event = {type, channel, key, pressure };
+			break;
+		case 0xB0:
+			var controller = view.getUint8(index++);
+			var value = view.getUint8(index++);
+
+			if (controller >= 120 && controller <= 127) {
+				console.info("Channel Mode Message");
+			}
+
+			event = { type, channel, controller, value };
+			break;
+		case 0xC0:
+			var program = view.getUint8(index++);
+
+			event = { type, channel, program };
+			break;
+		case 0xD0:
+			var pressure = view.getUint8(index++);
+
+			event = { type, channel, pressure };
+			break;
+		case 0xE0:
+			var value = view.getUint16(index);
+			index += 2;
+
+			event = { type, channel, value };
+			break;
+		case 0xF0:
+			switch (status) {
+				case 0xF0:
+				case 0xF7:
+					console.info("Sysex Events not supported");
+					break;
+				case 0xFF:
+					const metaType = view.getUint8(index++);
+					const length = view.getUint8(index++);
+
+					let text;
+					if (metaType !== 0x00 && metaType < 0x08) {
+						text = getAsciiText(view, index, length);
+					}
+
+					switch (metaType) {
+						case 0x00:
+							meta = { sequenceNumber: view.getUint16(index) };
+							break;
+						case 0x01:
+						case 0x05:
+						case 0x06:
+						case 0x07:
+							event = { type: status, metaType, text };
+							break;
+						case 0x02:
+							meta = { copyright: text };
+							break;
+						case 0x03:
+							meta = { name: text };
+							break;
+						case 0x04:
+							meta = { instrument: text };
+							break;
+						case 0x20:
+							const channel = view.getUint8(index);
+							meta = { channel };
+							break;
+						case 0x2F:
+							event = { type: status, metaType };
+							break;
+						case 0x51:
+							const tempoHI = view.getUint8(index);
+							const tempoLO = view.getUint16(index + 1);
+							const tempo = (tempoHI << 16) + tempoLO;
+							meta = { tempo };
+							break;
+						case 0x54:
+							const hours = view.getUint8(index);
+							const minutes = view.getUint8(index + 1);
+							const seconds = view.getUint8(index + 2);
+							const frames = view.getUint8(index + 3);
+							const fractions = view.getUint8(index + 4);
+							meta = { smpte: { hours, minutes, seconds, frames, fractions } };
+							break;
+						case 0x58:
+							const numerator = view.getUint8(index);
+							const denominator = view.getUint8(index + 1);
+							const clocksPerTick = view.getUint8(index + 2);
+							const notesPer24Clocks = view.getUint8(index + 3);
+							meta = { timeSignature: { numerator, denominator, clocksPerTick, notesPer24Clocks } };
+							break;
+						case 0x59:
+							const sharpsFlats = view.getUint8(index);
+							const majorMinor = view.getUint8(index + 1);
+							meta = { sharpsFlats, majorMinor };
+							break;
+						case 0x7F:
+							console.info("System Exclusive Message not supported");
+							break;
+					}
+					index += length;
+					break;
+				default:
+					throw new Error("Unsupported Event Type " +  "0x" + status.toString(16) + " at 0x" + index.toString(16));
+			}
+			break;
+		default:
+			throw new Error("Unsupported Event Type " + "0x" + status.toString(16) + " at 0x" + (index-1).toString(16));
+	}
+
+	return { event, meta, index };
 }
 
 function getAsciiText(dataView, index, length) {
